055a8390cc6269704d3eff1ab9549f2afc8e1cde
AlbaReverte <albareverteruiz@gmail.com>
2024-12-15 19:53:02 +0000

    StarWars rest api

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -20,6 +20,7 @@ if db_url is not None:
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 MIGRATE = Migrate(app, db)
 db.init_app(app)
@@ -36,16 +37,77 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all()
+    return jsonify([person.serialize() for person in people])
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_person(people_id):
+    person = People.query.get_or_404(people_id)
+    return jsonify(person.serialize())
 
-    return jsonify(response_body), 200
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    return jsonify([planet.serialize() for planet in planets])
+    
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get_or_404(planet_id)
+    return jsonify(planet.serialize())
 
-# this only runs if `$ python src/app.py` is executed
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.serialize() for user in users])
+
+@app.route('/users/favorites', methods=['GET'])
+def get_user_favorites():
+    user = User.query.first()
+    favorites = Favorite.query.filter_by(user_id = user.id).all()
+    return jsonify([favorite.serialize() for favorite in favorites])
+
+@app.route('/favorite/planets/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    user = User.query.first()
+    new_favorite = Favorite(user_id = user.id, planet_id = planet_id)
+    db.session.add(new_favorite)
+    db.session.commit()
+    return jsonify("Planeta favorito creado"), 201
+
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_favorite_people(people_id):
+    user = User.query.first()
+    new_favorite = Favorite(user_id = user.id, people_id = people_id)
+    db.session.add(new_favorite)
+    db.session.commit()
+    return jsonify("Personaje favorito creado"), 201
+
+
+@app.route('/favorite/planets/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet(planet_id):
+    user = User.query.first()
+    deleted_favorite = Favorite(user_id = user.id, planet_id = planet_id)
+    db.session.delete(deleted_favorite)
+    db.session.commit()
+    return jsonify("Planeta favorito borrado"), 200
+
+
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_favorite_people(people_id):
+    user = User.query.first()
+    deleted_favorite = Favorite(user_id = user.id, people_id = people_id)
+    db.session.delete(deleted_favorite)
+    db.session.commit()
+    return jsonify("Personaje favorito borrado"), 200
+
+   
+
+
+
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
